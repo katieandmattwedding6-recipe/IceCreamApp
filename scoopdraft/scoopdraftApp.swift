290c014390bb7549715c29cedb31f86e45406ea6
Matthew <stewartmatthewj@johndeere.com>
2026-07-08 13:19:02 -0500

    Initial Commit

---
+++ scoopdraft/scoopdraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  scoopdraftApp.swift
+//  scoopdraft
+//
+//  Created by Stewart Matthew J on 7/8/26.
+//
+
+import SwiftUI
+
+@main
+struct scoopdraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
