83962f72db92ff60cd489212dc674f6add64089c
Matthew <stewartmatthewj@johndeere.com>
2026-08-30 14:25:20 -0500

    Added form information for new score

---
+++ scoopdraft/Models/IceCreamDeliveryMethodEnum.swift
@@ -0,0 +1,14 @@
+//
+//  IceCreamDeliveryMethod.swift
+//  scoopdraft
+//
+//  Created by Stewart Matthew J on 8/22/26.
+//
+
+enum IceCreamDeliveryMethod: String, CaseIterable {
+    case sugarCone = "Sugar Cone"
+    case waffleCone = "Waffle Cone"
+    case iceCreamFloat = "Ice Cream Float"
+    case milkshake = "Milkshake"
+    case bowl = "Bowl"    
+}
--- scoopdraft/Views/AddNewScoreView.swift
+++ scoopdraft/Views/AddNewScoreView.swift
@@ -8,12 +8,62 @@
 import SwiftUI
 
 struct AddNewScoreView: View {
+    
+    //
+    //struct IceCreamScore: {
+    //    let id = UUID()
+    //    let userName: String
+    //
+    //    // Store details
+    //
+    //    // Ice cream info
+    //    let rating: Float
+    //    //let eatenDate: Date
+    //}
+    @State private var deliveryMethod = IceCreamDeliveryMethod.sugarCone
+    
+    @State private var shopName = ""
+    @State private var shopCity = ""
+    @State private var shopState = ShopState.alabama
+    @State private var flavorName = ""
+    
     var body: some View {
-        Text("Freezing base. Come back when it's ready!")
+        
+        Form {
+            
+            Section("Store Info") {
+                TextField("Shop name", text: $shopName)
+                TextField("Shop city", text: $shopCity)
+                Picker("Shop state", selection: $shopState) {
+                    ForEach(ShopState.allCases, id: \.self) {
+                        shopState in Text(shopState.rawValue).tag(shopState)
+                    }
+                }
+            }
+            
+            Section("Ice Cream Info") {
+                TextField("Flavor name", text: $flavorName)
+                Picker("Serving style", selection: $deliveryMethod) {
+                    ForEach(IceCreamDeliveryMethod.allCases, id: \.self) {
+                        method in Text(method.rawValue).tag(method)
+                    }
+                }
+            }
+            
+            Section("User Info") {
+                
+            }
+        }
     }
 }
 
 
 #Preview {
     AddNewScoreView()
+}
+
+struct Previews_AddNewScoreView_LibraryContent: LibraryContentProvider {
+    var views: [LibraryItem] {
+        LibraryItem(/*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/)
+    }
 }
